e16cc12872ce33749b823deac4f8c38893333060
bsairohit <bsairohit20@gmail.com>
2022-03-10 12:05:45 +0530

    final

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css'; 
 import 'mdbreact/dist/css/mdb.css';
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+    <App />,
+ 
   document.getElementById('root')
 );
 
