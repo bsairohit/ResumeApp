0bf3ab00ee8b892850704f84c50f4a6317192726
bsairohit <bsairohit20@gmail.com>
2022-03-10 12:36:53 +0530

    final

--- src/App.js
+++ src/App.js
@@ -4,23 +4,30 @@ import './App.css';
 import NavbarApp from './components/Navbar';
 
 import Footer from './components/Footer'
-import Resume from './components/Resume'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Resume  from './components/Resume';
 function App() {
   return (<>
-  <NavbarApp />
+    {/* <NavbarApp />
+    <CarouselApp />
+    <Footer /> */}
+    <Router>
+      <Routes>
+        <Route path="/" element={<> 
+        <NavbarApp />
+         <Resume/>
+          <Footer /></>} />
+          <Route path="" element={
+          <> 
+          <NavbarApp />
           <Resume />
           <Footer />
- 
-    {/* <Router>
-      <Routes>
-        <Route path="/" element={<> <NavbarApp />
-          <Resume />
-          <Footer /></>} />
-         
+          </>} />
       </Routes>
-    </Router> */}
+    </Router>
   </>);
 }
 
 export default App;
+ // "react-dom": "^17.0.2",
+    // "react": "^17.0.2",
--- src/index.js
+++ src/index.js
@@ -6,10 +6,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css'; 
 import 'bootstrap-css-only/css/bootstrap.min.css'; 
 import 'mdbreact/dist/css/mdb.css';
+import { BrowserRouter } from 'react-router-dom';
 ReactDOM.render(
   
-    <App />,
  
+    <React.StrictMode>
+  
+    <BrowserRouter>
+    <App />
+    </BrowserRouter>
+  
+     
+    </React.StrictMode>,
   document.getElementById('root')
 );
 
